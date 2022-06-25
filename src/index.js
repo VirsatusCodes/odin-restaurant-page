52f708418844307a95efced4b647181930fdca57
VirsatusCodes <donavynpence@yahoo.com>
2022-06-25 16:02:03 -0700

    reseting index and working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style.css';
-import * as contactPage from './contact-page-creation';
-import * as menuPage from './menu-page-creation';
-import * as initialPage from './initial-page-creation';
+import * as contactPage from './pages/contact-page-creation';
+import * as menuPage from './pages/menu-page-creation';
+import * as initialPage from './pages/initial-page-creation';
 /* npx webpack | npx webpack --watch | for the terminal*/
 
 console.log('module 1 test');
-
-contactPage .functionOne();
-contactPage .functionTwo();
